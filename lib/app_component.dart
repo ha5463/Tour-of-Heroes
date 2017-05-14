c49cffbcab3634c5bb8aae243ae9576aa76f76eb
Ajay Agarwal <ha5463@gmail.com>
2017-05-14 10:29:45 +0530

    Added Heroes to Dashboard

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,19 +1,27 @@
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
 
 import 'hero_service.dart';
 import 'heroes_component.dart';
+import 'package:firstWebApp/dashboard_component.dart';
 
 @Component(
     selector: 'my-app',
     template: '''
       <h1>{{title}}</h1>
-      <my-heroes></my-heroes>''',
-    directives: const [HeroesComponent],
-    providers: const [HeroService]
-)
+        <a [routerLink]="['Dashboard']">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+      <router-outlet></router-outlet>''',
+    directives: const [ROUTER_DIRECTIVES],
+    providers: const [HeroService, ROUTER_PROVIDERS])
+@RouteConfig(const [
+  const Route(path: '/heroes', name: 'Heroes', component: HeroesComponent),
+  const Route(path: '/dashboard', name: 'Dashboard', component: DashboardComponent, useAsDefault: true)
+])
 
 class AppComponent {
   String title = 'Tour of Heroes';
 
   String name;
 }
+
---
+++ lib/dashboard_component.dart
@@ -0,0 +1,20 @@
+import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+import 'dart:async';
+import 'package:firstWebApp/hero.dart';
+import 'package:firstWebApp/hero_service.dart';
+
+@Component(
+    selector: 'my-dashboard',
+    templateUrl: 'dashboard_component.html',
+    directives: const[ROUTER_DIRECTIVES]
+)
+class DashboardComponent implements OnInit{
+  List<Hero> heroes;
+
+  final HeroService  _heroservice;
+  DashboardComponent(this._heroservice);
+  Future<Null> ngOnInit() async{
+    heroes = (await _heroservice.getHeros()).skip(1).take(4).toList();
+  }
+}
